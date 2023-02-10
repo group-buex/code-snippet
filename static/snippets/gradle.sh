2663809a16eb4d1c6e9c60e9a67f2d6f2a4a76f9
group-buex <101394418+group-buex@users.noreply.github.com>
2023-02-10 22:33:31 +0900

    Update gradle.sh

--- static/snippets/gradle.sh
+++ static/snippets/gradle.sh
@@ -3,9 +3,10 @@
 
 set -euf -o pipefail
 
+# ppa 저장소 추가
 sudo apt-get -y install vi apt-transport-https dirmngr wget software-properties-common && sudo add-apt-repository ppa:cwchien/gradle
 
-# Install dependencies
+# Install dependencies & gradle 7이상 버전으로 설치
 sudo apt-get update && sudo apt-get install -y gradle
 
 gradle -v
