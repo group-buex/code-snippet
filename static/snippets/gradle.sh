04e72de950c3637171a6f2a2ea239f398965b2ed
group-buex <101394418+group-buex@users.noreply.github.com>
2023-02-10 22:53:54 +0900

    Update gradle.sh

--- static/snippets/gradle.sh
+++ static/snippets/gradle.sh
@@ -9,8 +9,7 @@ wget https://services.gradle.org/distributions/gradle-${VERSION}-bin.zip -P /tmp
 
 sudo unzip -d /opt/gradle /tmp/gradle-${VERSION}-bin.zip && sudo ln -s /opt/gradle/gradle-${VERSION} /opt/gradle/latest
 
-cat > /etc/profile.d/gradle.sh export GRADLE_HOME=/opt/gradle/latest
-export PATH=${GRADLE_HOME}/bin:${PATH}
+echo -e "export GRADLE_HOME=/opt/gradle/latest \nexport PATH=${GRADLE_HOME}/bin:${PATH}" | sudo tee /etc/profile.d/gradle.sh
 
 # 터미널에서 gradle 입력시 위에 설치한 버전으로 연결
 # Set Sxecutable Permission
