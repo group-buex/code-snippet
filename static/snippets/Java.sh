7d741c71462a0f2036258c3e11a9dbd0617f11db
group-buex <101394418+group-buex@users.noreply.github.com>
2023-02-10 20:58:03 +0900

    Update Java.sh

---
+++ static/snippets/Java.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+## INFO: https://docs.docker.com/engine/install/ubuntu/
+
+set -euf -o pipefail
+
+# Install dependencies
+sudo apt-get update && sudo apt-get install -y default-jdk
+
+java -version
