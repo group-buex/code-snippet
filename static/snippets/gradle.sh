ac128a6155fef2991fa2204a1825d64dcd8f1aa4
group-buex <101394418+group-buex@users.noreply.github.com>
2023-02-10 22:41:45 +0900

    Update gradle.sh

--- static/snippets/gradle.sh
+++ static/snippets/gradle.sh
@@ -3,10 +3,19 @@
 
 set -euf -o pipefail
 
-# ppa 저장소 추가
-sudo apt-get -y install vi apt-transport-https dirmngr wget software-properties-common && sudo add-apt-repository ppa:cwchien/gradle
+VERSION=7.1.1
 
-# Install dependencies & gradle 7이상 버전으로 설치
-sudo apt-get update && sudo apt-get install -y gradle
+wget https://services.gradle.org/distributions/gradle-${VERSION}-bin.zip -P /tmp
+
+sudo unzip -d /opt/gradle /tmp/gradle-${VERSION}-bin.zip && sudo ln -s /opt/gradle/gradle-${VERSION} /opt/gradle/latest
+
+cat >> /etc/profile.d/gradle.sh export GRADLE_HOME=/opt/gradle/latest
+export PATH=${GRADLE_HOME}/bin:${PATH}
+
+# 터미널에서 gradle 입력시 위에 설치한 버전으로 연결
+# Set Sxecutable Permission
+sudo chmod +x /etc/profile.d/gradle.sh
+# Script Loading
+source /etc/profile.d/gradle.sh
 
 gradle -v
